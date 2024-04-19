62b3b69353939a3aa51c891c312b8c4a3e19d08d
ckdwns1221 <ckdwns2181@gmail.com>
2024-04-20 05:29:15 +0900

    feat: checkerFile 기능 구현, upload UI 수정

--- src/pages/Checker/Checker.jsx
+++ src/pages/Checker/Checker.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'; // HTTP 요청을 위한 axios 라이브러리
 import { useNavigate } from 'react-router-dom'; // 페이지 이동
 import CheckerFile from './CheckerFile';
 import CheckerModify from './CheckerModify';
+import { images } from '../../utils/images';
 
 function Checker() {
   const navigate = useNavigate();
@@ -62,7 +63,16 @@ function Checker() {
       <Nav />
       <div className="min-h-screen">
         <div className="w-10/12 bg-white rounded-xl shadow-md flex flex-col mx-auto items-center mt-24 p-4">
-          <div className="text-3xl fontBold w-11/12 border-l-8 border-[#303A6E] pl-4 py-3 my-4">맞춤법 검사</div>
+          <div className="text-3xl fontBold w-11/12 border-l-8 border-[#303A6E] pl-4 py-3 mt-4 flex justify-between">
+            <div>맞춤법 검사</div>
+          </div>
+          <div className="flex items-center w-11/12 px-1 justify-between text-base text-[#a9a9a9]">
+            <div className="text-sm">** 본 내용에서 서식은 무시됩니다.</div>
+            <div className="flex items-center cursor-pointer" onClick="">
+              <img src={images.Question} alt="물음표 아이콘" className="w-4 h-4 mx-2" />
+              <div className="text-[#c8c8c8]">사용설명서</div>
+            </div>
+          </div>
           <div className="flex justify-center w-11/12 h-full mt-2">
             <CheckerFile />
             <CheckerModify />
@@ -70,6 +80,7 @@ function Checker() {
           <div className="w-11/12 mt-4 flex justify-end items-center">
             <button
               className="text-sm text-white w-1/12 py-2 bg-slate-700 fontBold rounded-2xl mr-4"
+              s
               onClick={finishEdit}>
               수정 완료
             </button>
--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Nav from '../../components/Nav';
+import { images } from '../../utils/images';
 
 function Upload() {
   const [inputType, setInputType] = useState('file'); // 입력 유형 상태 관리 ('file' 또는 'text')
@@ -17,43 +18,37 @@ function Upload() {
   return (
     <>
       <Nav />
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="w-10/12 h-4/5 bg-white rounded-xl shadow flex flex-col items-center pt-20">
-          {/* 문서 업로드 제목 */}
-          <div className="w-10/12">
-            <div className="h-16 bg-white border-l-8 border-slate-700 flex items-center pl-6">
-              <div className="text-4xl fontBold">문서 업로드</div>
+      <div className="min-h-screen">
+        <div className="w-10/12 bg-white rounded-xl shadow-md flex flex-col mx-auto items-center mt-24 p-4">
+          <div className="text-3xl fontBold w-11/12 border-l-8 border-[#303A6E] pl-4 py-3 mt-4 flex justify-between">
+            <div>문서 업로드</div>
+          </div>
+          <div className="flex items-center w-11/12 px-1 justify-end text-base text-[#c8c8c8]">
+            <div className="flex items-center cursor-pointer" onClick="">
+              <img src={images.Question} alt="물음표 아이콘" className="w-4 h-4 mx-2" />
+              <div>사용설명서</div>
             </div>
           </div>
-          {/* 사용메뉴얼 */}
-          <div className="w-10/12 mt-8 flex justify-end">
-            <div className="flex items-center">
-              <img src="./assets/images/QuestionMark.png" alt="사용메뉴얼" className="h-5 mr-1" />
-              <span className="text-stone-300 text-l leading-normal">사용메뉴얼</span>
-            </div>
-          </div>
-          {/* 상단 border */}
-          <div className="w-10/12 border-2 border-slate-700 mt-4"></div>
-          {/* 중앙 박스 */}
-          <div className="flex w-10/12 mt-12 mb-12 ml-20">
-            <div className="flex justify-between w-full items-center">
+          <div className="flex w-11/12 justify-center p-12 my-4 border-t-4 border-b-4 border-slate-700">
+            <div className="flex justify-evenly w-full items-center">
               {inputType === 'file' ? (
-                <label className="w-80 h-80 bg-neutral-50 rounded-[10px] border border-dashed border-neutral-400 flex justify-center items-center cursor-pointer">
-                  <input type="file" className="hidden" onChange={handleFileChange} />
-                  <div className="flex flex-col items-center">
-                    {/* 파일이 선택되었을 경우 파일 이름을 표시하거나, 기본 메시지를 보여줍니다. */}
-                    {selectedFile ? (
-                      <div className="text-neutral-400 text-xl">{selectedFile.name}</div>
-                    ) : (
-                      <>
-                        <img src="./assets/images/file_upload.png" alt="파일업로드" className="size-16" />
-                        <div className="mt-5 text-neutral-400 text-xl">Drag file to upload</div>
-                      </>
-                    )}
-                  </div>
-                </label>
+                <div className="w-1/2 flex justify-center">
+                  <label className="w-80 h-80 bg-neutral-50 rounded-[10px] border border-dashed border-neutral-400 flex justify-center items-center cursor-pointer">
+                    <input type="file" className="hidden" onChange={handleFileChange} />
+                    <div className="flex flex-col items-center">
+                      {selectedFile ? (
+                        <div className="text-neutral-400 text-xl">{selectedFile.name}</div>
+                      ) : (
+                        <>
+                          <img src="./assets/images/file_upload.png" alt="파일업로드" className="size-16" />
+                          <div className="mt-5 text-neutral-400 text-xl">Drag file to upload</div>
+                        </>
+                      )}
+                    </div>
+                  </label>
+                </div>
               ) : (
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center w-1/2">
                   <div className="w-80 flex flex-col bg-white border border-zinc-300">
                     <div className="bg-stone-50 text-center p-2 border border-zinc-300 text-sm">문서 내용</div>
                     <div className="flex flex-col justify-center items-center">
@@ -68,53 +63,33 @@ function Upload() {
                   <div className="text-right w-80">{`${text.length}/1000자`}</div>
                 </div>
               )}
-              {/* 검사 정보 목록 */}
-              <div className="flex flex-col ml-8 w-3/5">
-                {/* 검사명 */}
+              <div className="flex flex-col w-1/2">
                 <div className="flex flex-col pb-10">
                   <div className="flex items-center">
                     <img src="./assets/images/list_disc.png" alt="리스트 원" className="mr-4" />
-                    <span className="text-lg font-medium">검사명</span>
-                    <input className="ml-10 p-1 h-8 bg-white border border-zinc-400 w-2/4" />
+                    <span className="text-lg font-medium w-14">검사명</span>
+                    <input className="p-1 h-8 bg-white border border-zinc-400 rounded w-7/12 ml-10" />
                   </div>
-                  <div className="ml-28 mt-4 text-neutral-400 text-opacity-95 text-xs font-bold">
-                    * 파일 다운 시 작성하신 검사명으로 파일 명이 작성됩니다..
+                  <div className="ml-28 pl-2 mt-4 text-neutral-400 text-opacity-95 text-xs">
+                    * 파일 다운 시 작성하신 검사명으로 파일 명이 작성됩니다.
                   </div>
                 </div>
-                {/* 검사 유형 */}
                 <div className="flex items-center pb-4">
                   <img src="./assets/images/list_disc.png" alt="리스트 원" className="mr-4" />
                   <span className="text-lg font-medium">검사 유형</span>
-                  {/* 라디오 버튼 */}
-                  <div className="flex ml-6 items-center">
-                    <div className="flex items-center mr-4">
-                      <input type="radio" id="BUSAN_UNIV" name="spellCheckService" value="BUSAN_UNIV" />
-                      <label htmlFor="BUSAN_UNIV" className="ml-2">
-                        부산대 맞춤법 검사기
-                      </label>
-                    </div>
-                    <div className="flex items-center mr-4">
-                      <input type="radio" id="INCRUIT" name="spellCheckService" value="INCRUIT" />
-                      <label htmlFor="INCRUIT" className="ml-2">
-                        인크루트 맞춤법 검사기
-                      </label>
-                    </div>
-                    <div className="flex items-center">
-                      <input type="radio" id="JOB_KOREA" name="spellCheckService" value="JOB_KOREA" />
-                      <label htmlFor="JOB_KOREA" className="ml-2">
-                        잡코리아 맞춤법 검사기
-                      </label>
-                    </div>
-                  </div>
+                  <select name="" id="" className="ml-6 border border-zinc-400 rounded p-1">
+                    <option value="">선택</option>
+                    <option value="">부산대 맞춤법 검사기</option>
+                    <option value="">인크루트 맞춤법 검사기</option>
+                    <option value="">잡코리아 맞춤법 검사기</option>
+                  </select>
                 </div>
-                {/* 검사설정 */}
                 <div className="flex items-center w-full mt-5">
                   <div className="flex items-center">
                     <img src="./assets/images/list_disc.png" alt="리스트 원" className="mr-4" />
                     <span className="text-lg font-medium">검사설정</span>
                   </div>
                   <div className="flex items-center">
-                    {/* 파일, 텍스트 라디오 버튼 */}
                     <div className="flex items-center ml-7">
                       <input
                         type="radio"
@@ -143,8 +118,8 @@ function Upload() {
                     </div>
                   </div>
                 </div>
-                <div className="text-neutral-400 text-sm font-bold font-['Epilogue'] leading-normal pt-5 pl-28">
-                  <div className="mb-2">
+                <div className="text-neutral-400 text-xs leading-normal pt-2 pl-28 ml-1">
+                  <div className="font-bold">
                     * 파일 참고란
                     <br />
                   </div>
@@ -160,12 +135,9 @@ function Upload() {
               </div>
             </div>
           </div>
-          {/* 하단 border */}
-          <div className="w-10/12 border-2 border-slate-700"></div>
-          {/* 검사시작 및 닫기 버튼 */}
-          <div className="w-10/12 mt-4 flex justify-end items-center">
+          <div className="w-11/12 mt-1 flex justify-end items-center">
             <button className="text-white w-32 h-10 bg-slate-700 font-medium rounded-2xl mr-4">검사 시작</button>
-            <button className="w-32 h-10 bg-zinc-100 rounded-2xl border border-stone-300">닫기</button>
+            <button className="w-32 h-10 bg-zinc-100 rounded-2xl border border-stone-300">이전 화면</button>
           </div>
         </div>
       </div>
